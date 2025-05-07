b38927674d156a0d198f11c2270d0ad92f8bba0b
TilawatArnaud <tilawatarnaud@gmail.com>
2025-05-07 14:15:00 +0200

    Change case if one argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
         print("0 arguments")
-    elif len(sys.argv) == 1:
+    elif len(sys.argv) == 2:
         print("1 argument:")
-        print(f"1: argument {sys.argv}")
+        print(f"1: {sys.argv[1]}")
     else:
         print(f"{len(sys.argv[1:])} arguments:")
         for x in range (len(sys.argv[1:])):
