8e44a851eca9b30350fb8f1523e86d98c45b4120
TilawatArnaud <tilawatarnaud@gmail.com>
2025-05-07 14:16:31 +0200

    added . to 0 arg and pycodestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,11 +3,11 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("0 arguments")
+        print("0 arguments.")
     elif len(sys.argv) == 2:
         print("1 argument:")
         print(f"1: {sys.argv[1]}")
     else:
         print(f"{len(sys.argv[1:])} arguments:")
-        for x in range (len(sys.argv[1:])):
-            print(f"{x +1}: {sys.argv[x+1]}")
+        for x in range(len(sys.argv[1:])):
+            print(f"{x + 1}: {sys.argv[x + 1]}")
