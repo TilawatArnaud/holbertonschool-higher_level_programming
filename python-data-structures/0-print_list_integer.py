b14d0feffbb74ce8a0158f580c2990a1454af555
TilawatArnaud <tilawatarnaud@gmail.com>
2025-05-12 11:46:01 +0200

    0-print_list_integer.py

--- python-data-structures/0-print_list_integer.py
+++ python-data-structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def print_list_integer(mylist=[]):
-    for x in mylist:
-        print({},str.format(mylist[x]))
+def print_list_integer(my_list=[]):
+    for x in range (len(my_list)):
+        print("{}".format(my_list[x]))
