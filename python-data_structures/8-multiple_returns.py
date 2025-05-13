72e9706fe4477d75a6f40d4237442cf6dda802e0
TilawatArnaud <tilawatarnaud@gmail.com>
2025-05-13 14:31:26 +0200

    pycodestyle

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if len(sentence) == 0:
-        return (length, None)   
+        return (length, None)
     else:
         first = sentence[0]
         return (length, first)
