46a9aa58d63124d30f3055eff492249b3032140a
TilawatArnaud <tilawatarnaud@gmail.com>
2025-05-14 14:31:27 +0200

    4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -15,6 +15,9 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print("wrong type")
             result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
         finally:
             continue
     return result
