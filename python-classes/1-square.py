e2bc2c09d25f5a03fc442c5a3d120a3031260bc9
TilawatArnaud <tilawatarnaud@gmail.com>
2025-05-19 17:39:43 +0200

    Start of the project and added 2 first tasks

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Class that defines a square"""
+    def __init__(self, size):
+        """Initialize the square with a private size attribute"""
+        self.__size = size
