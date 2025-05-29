d8f83496882cbef79355d0022a66ebbc039a464a
TilawatArnaud <tilawatarnaud@gmail.com>
2025-05-29 14:33:16 +0200

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry with area method
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry Class"""
+    def area(self):
+        raise Exception("area() is not not implemented")
