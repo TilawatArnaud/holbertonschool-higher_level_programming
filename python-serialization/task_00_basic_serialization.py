8c2f1a20e22dae7f9f68d268f1c292720809132e
TilawatArnaud <tilawatarnaud@gmail.com>
2025-06-03 14:50:13 +0200

    Start of the project and task 1

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def serialization_and_save_tofile(data, filename):
+    """
+    Serialize data to a file using JSON format.
+
+    Args:
+        data: The data to serialize
+        filename (str): The name of the file to save the serialized data to
+    """
+    import json
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """
+    Deserialize data from a file using JSON format.
+
+    Args:
+        filename (str): The name of the file to load the serialized data from
+
+    Returns:
+        The deserialized data
+    """
+    import json
+    with open(filename, 'r') as f:
+        return json.load(f)
