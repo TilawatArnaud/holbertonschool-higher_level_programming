9f861cd73dadc2e1756026a998ab60cc39dfe0d8
TilawatArnaud <tilawatarnaud@gmail.com>
2025-06-03 12:47:23 +0200

    2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Modules for append write
+"""
+
+def append_write(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns character count.
+
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The text to write to the file.
+
+    Returns:
+        int: The number of characters written.
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        chars_written = f.write(text)
+    return chars_written
