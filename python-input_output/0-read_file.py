a64693f6d00e704ea2f1ff615d34dd796cfa79b6
TilawatArnaud <tilawatarnaud@gmail.com>
2025-06-03 11:21:36 +0200

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -18,5 +18,5 @@ def read_file(filename=""):
     Returns:
         None
     """
-    with open(filename, 'r') as file:
-        print(file.read(), end="")
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read())
