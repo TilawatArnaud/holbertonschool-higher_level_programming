e292823e7bb909e187517d7b996d7ecce9727841
TilawatArnaud <tilawatarnaud@gmail.com>
2025-06-03 12:01:42 +0200

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""
-Function for write a filetext
-"""
+"""Module for writing strings to files."""
+
 
 def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns character count.
+
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The text to write to the file.
+
+    Returns:
+        int: The number of characters written.
     """
-    Function for write a filetext
-    """
-    with open(filename, "w", encoding="UTF-8") as the_file:
-        the_file.write(text)
-    return len(text)
+    with open(filename, 'w', encoding='utf-8') as f:
+        chars_written = f.write(text)
+    return chars_written
