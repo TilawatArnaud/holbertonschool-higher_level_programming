28efa5449e8a7b6d8e7b321595066fc7c9f7b86c
TilawatArnaud <tilawatarnaud@gmail.com>
2025-06-03 14:38:36 +0200

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Module for Student class
+"""
+
+
+class Student:
+    """
+    Class for Student
+    """
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize the Student object with first name, last name, and age.
+
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the dictionary description with simple data structure
+        (list, dictionary, string, integer and boolean)
+        for JSON serialization of an object.
+
+        Args:
+            obj: An instance of a class with serializable attributes
+
+        Returns:
+            A dictionary containing all serializable attributes of the object
+        """
+        result = {}
+        for key, value in self.__dict__.items():
+            if attrs is not None and key not in attrs:
+                continue
+            if isinstance(value, (int, str, bool, list, dict)):
+                result[key] = value
+            elif key.startswith('_') and not key.startswith('__'):
+                result[key] = value
+        return result
