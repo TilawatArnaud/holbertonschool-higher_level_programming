bbeeef1eba2ac1144709b1e743607e1c6b8e8fb8
TilawatArnaud <tilawatarnaud@gmail.com>
2025-06-12 14:22:39 +0200

    added {}

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -31,7 +31,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            message = "OK"
+            message = {"OK"}
             self.wfile.write(json.dumps(message).encode('utf-8'))
             return
 
@@ -50,7 +50,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            message = "Endpoint not found"
+            message = {"Endpoint not found"}
             self.wfile.write(json.dumps(message).encode('utf-8'))
 
 
