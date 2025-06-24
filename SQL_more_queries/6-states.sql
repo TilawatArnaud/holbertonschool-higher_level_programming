b95d0e495e8aef7e649343dfd1ca24ae749c9523
TilawatArnaud <tilawatarnaud@gmail.com>
2025-06-24 13:59:48 +0200

    Fix: Add USE statement and improve PRIMARY KEY syntax

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
 -- Create a database and a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
