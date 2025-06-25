54ffc37c5476de6504d2a80698cfc34aec8124fd
TilawatArnaud <tilawatarnaud@gmail.com>
2025-06-25 12:46:01 +0200

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,5 @@
 -- Lists all shows contained in the database that have at least one genre linked
-SELECT DISTINCT T1.id, T1.name FROM shows AS T1 JOIN genre_show AS T2 ON T1.id = T2.show_id ORDER BY T1.id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
