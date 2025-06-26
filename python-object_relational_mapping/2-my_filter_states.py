2e32d53a1b642ec3030dcfc612a7605a14940044
TilawatArnaud <tilawatarnaud@gmail.com>
2025-06-26 14:17:21 +0200

    removed match from connect

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        match=sys.argv[4],
     )
+    match = sys.argv[4]
 
     # Create cursor object using the cursor() method
     cursor = db.cursor()
